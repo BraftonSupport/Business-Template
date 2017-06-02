8ee882d08f330177a711d4c6fbb68b0aff234f2c
Yvonne Tse <yvonne.tse@brafton.com>
2017-06-02 16:07:25 -0400

    add video

--- frontpage-parts/visual-template.php
+++ frontpage-parts/visual-template.php
@@ -12,6 +12,7 @@ $shadow = get_field('shadow', $id);
 $bgc = get_field('background_color', $id);
 $tc = get_field('text_color', $id);
 
+$video = get_field('video');
 $visual_intro_text = get_field('visual_intro_text');
 $visual_button_text = get_field('visual_button_text');
 $visual_button_link = get_field('visual_button_link');
@@ -19,11 +20,26 @@ $visual_button_link = get_field('visual_button_link');
 $tracking = get_field('tracking');
 ?>
 
-<section id="post-<?php the_ID(); ?>" <?php post_class('visual'); ?> style="<?php
+<section id="post-<?php the_ID(); ?>" <?php
+	if ($video) { post_class('visual video');
+	} else { post_class('visual'); } ?> style="<?php
 	if ( !empty($url) ) { echo 'background-image: url('. $url .');'; }
 	if ( !empty($bgc) ) { echo ' background-color:'. $bgc .';'; }
 	if ( !empty($tc) ) { echo ' color:'. $tc .';'; }
 	?>">
+	<?php if ( $video ) {
+		if (strpos($video, '.webm') == false && strpos($video, '.mp4') == false) {
+			echo 'not webm or mp4';
+		} else {
+			$vidstring = chop($video, '.webm');
+			$vidstring = chop($vidstring, '.mp4'); ?>
+			<button id="vidpause"><i class="fa fa-pause" aria-hidden="true"></i></button>
+			<video playsinline autoplay muted loop id="bgvid">
+				<source src="<?php echo $vidstring; ?>.webm" type="video/webm">
+				<source src="<?php echo $vidstring; ?>.mp4" type="video/mp4">
+			</video>
+		<?php }
+	} ?>
 
 	<?php the_title( '<h1>', '</h1>' );
 	if ( $visual_intro_text ) { echo $visual_intro_text; }
@@ -32,11 +48,11 @@ $tracking = get_field('tracking');
 	}
 
 	wp_link_pages( array(
-		'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'expanse' ) . '</span>',
-		'after'       => '</div>',
+		'before'	  => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'expanse' ) . '</span>',
+		'after'	   => '</div>',
 		'link_before' => '<span>',
 		'link_after'  => '</span>',
-		'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'expanse' ) . ' </span>%',
+		'pagelink'	=> '<span class="screen-reader-text">' . __( 'Page', 'expanse' ) . ' </span>%',
 		'separator'   => '<span class="screen-reader-text">, </span>',
 	) ); ?>
 
@@ -51,3 +67,28 @@ $tracking = get_field('tracking');
 	); ?>
 </section><!-- section -->
 <?php if ( $shadow ) { echo '<div class="shadow"></div>'; } ?>
+<?php if ( $video ) { ?>
+	<script>
+	var vid = document.getElementById("bgvid"),
+	pauseButton = document.getElementById("vidpause");
+	function vidFade() {
+		vid.classList.add("stopfade");
+	}
+	vid.addEventListener('ended', function() {
+		// only functional if "loop" is removed 
+		 vid.pause();
+		// to capture IE10
+		vidFade();
+	});
+	pauseButton.addEventListener("click", function() {
+		vid.classList.toggle("stopfade");
+		if (vid.paused) {
+	vid.play();
+			pauseButton.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+		} else {
+			vid.pause();
+			pauseButton.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+		}
+	})
+	</script>
+<?php }

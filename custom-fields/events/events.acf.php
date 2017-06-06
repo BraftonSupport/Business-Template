089ef67397b0f1e98e945988799f16bba90f999b
dking3876 <dking3876@msn.com>
2017-06-06 12:59:18 -0400

    acf fields added

---
+++ custom-fields/events/events.acf.php
@@ -0,0 +1,46 @@
+<?php
+	register_field_group(array (
+		'id' => 'acf_events',
+		'title' => 'Events',
+		'fields' => array (
+			array (
+				'key' => 'field_5925f6ba0e510',
+				'label' => 'Date',
+				'name' => 'date',
+				'type' => 'date_picker',
+				'date_format' => 'yymmdd',
+				'display_format' => 'mm/dd/yy',
+				'first_day' => 0,
+			),
+			array (
+				'key' => 'field_5925f7ba0e511',
+				'label' => 'Location',
+				'name' => 'location',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'events',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'side',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
---
+++ custom-fields/style/style.acf.php
@@ -0,0 +1,50 @@
+<?php
+register_field_group(array (
+		'id' => 'acf_style',
+		'title' => 'Style',
+		'fields' => array (
+			array (
+				'key' => 'field_5923539920a13',
+				'label' => 'Background Color',
+				'name' => 'background_color',
+				'type' => 'color_picker',
+				'default_value' => '',
+			),
+			array (
+				'key' => 'field_592353db58a4c',
+				'label' => 'Text Color',
+				'name' => 'text_color',
+				'type' => 'color_picker',
+				'default_value' => '',
+			),
+			array (
+				'key' => 'field_592353b320a14',
+				'label' => 'Shadow',
+				'name' => 'shadow',
+				'type' => 'checkbox',
+				'choices' => array (
+					'shadow' => 'Add Shadow',
+				),
+				'default_value' => '',
+				'layout' => 'horizontal',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'page_type',
+					'operator' => '==',
+					'value' => 'child',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'side',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 5,
+	));

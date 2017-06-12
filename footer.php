aa3902f9ba3e5e50968ba2df578cbd5b9d2f571c
Yvonne Tse <yvonne.tse@brafton.com>
2017-06-12 12:11:17 -0400

    css mobile resp

--- footer.php
+++ footer.php
@@ -15,6 +15,29 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<?php if ( is_active_sidebar( 'footer-left' )||is_active_sidebar( 'footer-middle' )||is_active_sidebar( 'footer-right' ) ) :
 			echo '<div class="site-inner container">';
+				if ( get_theme_mod( 'expanse_logo' ) ) { ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="logo">
+				<?php
+					// set the image url
+					$image_url = esc_url( get_theme_mod( 'expanse_logo' ) );
+								 
+					// store the image ID in a var
+					$image_id = expanse_get_image_id($image_url);
+								 
+					// retrieve the thumbnail size of our image
+					$image_thumb = wp_get_attachment_image_src($image_id, 'medium'); ?>
+
+					<img src='<?php echo $image_thumb[0]; ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
+				</a>
+			<?php } else { ?>
+				<p><strong><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></strong></p>
+			<?php }
+
+			wp_nav_menu( array(
+				'theme_location' => 'social',
+				'menu_class'     => 'social-navigation',
+			) );
+
 			dynamic_sidebar( 'footer-left' );
 			dynamic_sidebar( 'footer-middle' );
 			dynamic_sidebar( 'footer-right' );

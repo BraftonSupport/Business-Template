d179037f26f996215c0e1afdad7b49a24d92d03c
Yvonne Tse <yvonne.tse@brafton.com>
2017-08-04 15:23:20 -0400

    footer css grid-ified!

--- footer.php
+++ footer.php
@@ -14,7 +14,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<?php if ( is_active_sidebar( 'footer-left' )||is_active_sidebar( 'footer-middle' )||is_active_sidebar( 'footer-right' ) ) :
-			echo '<div class="site-inner container">';
 				if ( get_theme_mod( 'expanse_logo' ) ) { ?>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="logo">
 				<?php
@@ -30,21 +29,22 @@
 					<img src='<?php echo $image_thumb[0]; ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
 				</a>
 			<?php } else { ?>
-				<p><strong><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></strong></p>
+				<p class="logo"><strong><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></strong></p>
 			<?php }
 
-			// wp_nav_menu( array(
-			// 	'theme_location' => 'social',
-			// 	'menu_class'     => 'social-navigation',
-			// ) );
-
+			wp_nav_menu( array(
+				'theme_location' => 'social',
+				'menu_class'     => 'social-navigation',
+			) );
+			
+			echo '<div class="container">';
 			dynamic_sidebar( 'footer-left' );
 			dynamic_sidebar( 'footer-middle' );
 			dynamic_sidebar( 'footer-right' );
-			echo '</div><br class="clear"/>';
+			echo '</div>';
 		endif; ?>
 			
-		<div class="site-info"><div class="site-inner container">
+		<div class="site-info">
 			<?php
 				/**
 				 * Fires before the expanse footer text for footer customization.
@@ -53,9 +53,8 @@
 				 */
 				do_action( 'expanse_credits' );
 			?>
-			<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></a></span>
-			<br class="clear"/>
-		</div></div><!-- .site-info -->
+			<div class="site-inner site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></a></div>
+		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
 </div><!-- .site -->
 

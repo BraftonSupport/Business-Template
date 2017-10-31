e07e7b58af1aa440b6f13e74c456ecf627f26e45
Yvonne Tse <yvonne.tse@brafton.com>
2017-10-31 18:20:53 -0400

    i'm not sure

--- inc/customizer.php
+++ inc/customizer.php
@@ -699,6 +699,7 @@ function businesstheme_get_color_scheme_css( $colors ) {
 	.simple .entry-footer a,
 	body:not(.search-results) article:first-of-type:not(.type-page).simple .entry-footer a,
 	header .top a,
+	.site-header.scrolled .primary-menu > li > a,
 	.site-footer a,
 	.social-navigation a,
 	.social-navigation a:hover,
@@ -707,6 +708,7 @@ function businesstheme_get_color_scheme_css( $colors ) {
 	}
 
 	.simple .entry-footer a:hover,
+	.site-header.scrolled .primary-menu > li > a:hover,
 	body:not(.search-results) article:first-of-type:not(.type-page).simple .entry-footer a:hover,
 	header .top a:hover,
 	.site-footer a:hover{
@@ -725,7 +727,6 @@ function businesstheme_get_color_scheme_css( $colors ) {
 	input[type="email"],
 	input[type="url"],
 	input[type="password"],
-	input[type="search"],
 	select,
 	textarea,
 	.main-navigation li,
@@ -919,7 +920,6 @@ function businesstheme_main_text_color_css() {
 		input[type="email"],
 		input[type="url"],
 		input[type="password"],
-		input[type="search"],
 		select,
 		textarea,
 		.main-navigation li,
@@ -1289,7 +1289,8 @@ function businesstheme_secondary_link_color_css() {
 		.social-navigation a,
 		.site-footer a,
 		.social-navigation a:hover,
-		.social-navigation a:hover::before {
+		.social-navigation a:hover::before,
+		.site-header.scrolled .primary-menu > li > a {
 			color: %1$s;
 		}
 	';
@@ -1327,7 +1328,8 @@ function businesstheme_secondary_link_hover_color_css() {
 		.simple .entry-footer a:hover,
 		body:not(.search-results) article:first-of-type:not(.type-page).simple .entry-footer a:hover,
 		header .top a:hover,
-		.site-footer a:hover{
+		.site-footer a:hover,
+		.site-header.scrolled .primary-menu > li > a:hover {
 			color: %1$s;
 		}
 	';
